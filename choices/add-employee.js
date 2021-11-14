605e99f8ba824fecaff22e01b826cd428b53f924
briandgomez <briandgomez98@gmail.com>
2021-11-14 18:24:05 -0500

    add-employee

---
+++ choices/add-employee.js
@@ -0,0 +1,62 @@
+const db = require('../db/connection');
+const questions = require('../questions');
+const inquirer = require('inquirer');
+
+const add_employee = () => {
+    const sql = `SELECT * FROM employee;`;
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        //console.log(rows);
+        const employeeArray = [];
+        rows.forEach(function (employee) {
+            employeeArray.push({ name: employee.department_name, value: employee.id });
+        });
+
+        const managerArray = [];
+        rows.forEach(function (manager) {
+            managerArray.push({ name: manager.first_name, value: manager.id });
+        });
+
+        //console.log(employeeArray);
+        console.log(managerArray);
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'first',
+                message: 'What is their first name?'
+            },
+            {
+                type: 'input',
+                name: 'last',
+                message: 'What is their last name?'
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What is their role?',
+                choices: employeeArray
+            },
+            {
+                type: 'list',
+                name: 'manager_id',
+                message: 'Who is their manager?',
+                choices: managerArray
+            }
+        ])
+            .then(response => {
+                console.log(response);
+                const sql = `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('${response.first}', '${response.last}',${response.role}, ${response.manager_id});`;
+                db.query(sql, (err, rows) => {
+                    if (err) throw err;
+                    const sql = `SELECT * FROM employee;`;
+                    db.query(sql, (err, rows) => {
+                        if (err) throw err;
+                        console.table(rows);
+                    });
+                });
+            })
+    });
+
+}
+
+module.exports = add_employee;
